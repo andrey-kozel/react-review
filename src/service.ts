45b168eb9a237250ebcc1d1664132a9d31f0821f
Andrey Kozel <andrey.kozel@spanning.com>
2023-03-06 11:51:05 +0300

    Improved code

--- src/service.ts
+++ src/service.ts
@@ -1,9 +1,9 @@
 class ItemsService {
 
-  itemsCount = 100;
+  itemsCount = 500;
 
   async getItems(filter: string, size?: number, page?: number): Promise<any> {
-    const actualSize = size || 20;
+    const actualSize = size || 100;
     const actualPage = page || 1;
     const remainingItems = this.itemsCount - (actualSize * (actualPage - 1));
     const itemsToReturn = remainingItems > actualSize ? actualSize : remainingItems;
@@ -12,10 +12,19 @@ class ItemsService {
       .then(() => {
         return Array.from({ length: itemsToReturn }, (_, index) => ({
           id: this.itemsCount - remainingItems + index,
-          title: `Item ${index}`
+          title: `Item ${index}`,
+          price: index * 10
         }))
           .filter(({ title }) => title.indexOf(filter) > -1);
       });
+  }
+
+  async getUserInfo(): Promise<any> {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return {
+      name: 'User',
+      age: 25
+    }
   }
 
 }
